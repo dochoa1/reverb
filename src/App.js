3fcb28c2727d6bf3ac03492302061309bc0dc45a
Danny Ochoa <danny@dannyochoa.net>
2024-05-04 21:31:29 -0400

    Simplifies the project.
    Main changes here include removing references to create-react-app since its no longer maintained.
    Also removed tests and code specific to the create-react-app default.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <meta charSet="utf-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+        <main>
+          <div className="App-header">
+            Hello
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
