9fe089da24a4dcd7ea40346fcdceb83620041b2d
Danny Ochoa <danny@dannyochoa.net>
2024-06-17 19:34:59 -0400

    Introduce some icons for my socials

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Socials from './components/socials/socials'
 import './App.css';
 
 class App extends Component {
@@ -9,9 +10,11 @@ class App extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         <main>
           <div className="App-header">
-            Hello
+            Welcome.
           </div>
+          <Socials/>
         </main>
+        {/* <script src="https://kit.fontawesome.com/a578134838.js" crossorigin="anonymous"></script> */}
       </div>
     );
   }
