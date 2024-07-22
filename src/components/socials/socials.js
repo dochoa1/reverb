5050ba6862ecc1a44c491097d354862824b9baab
Danny Ochoa <danny@dannyochoa.net>
2024-07-21 23:32:22 -0400

    Another step towards testing

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -5,7 +5,7 @@ class Socials extends Component {
   render() {
     return (
       <div className="socials">
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous"></link>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossOrigin="anonymous"></link>
           <div id="socials-list">
             <a href="mailto:danny@dannyochoa.net" id="mail"><i className="fas fa-envelope fa-2x font-awesome"></i></a>
             <a href="https://github.com/dochoa1" id="github" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-2x font-awesome"></i></a>
--- src/components/socials/tests/socials.test.js
+++ src/components/socials/tests/socials.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
-import Socials from "./socials";
+// import '@testing-library/jest-dom' // Why do the tutorials that I was follow recommend this?
+import Socials from "../socials";
 
 describe("Socials component", () => {
     it("should render Socials component correctly", () => {
         render(<Socials />);
-        const element = screen.getByRole("heading")  // TODO: Understand what this line is doing
+        // const element = screen.getByRole('link', {})  // TODO: Get this right
         expect(element).toBeInTheDocument();
     });
 });
